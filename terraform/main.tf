5ec454a3f6f78e407f770534160adf965c82bc2a
Christian Roggia <christian.roggia@gmail.com>
2020-02-25 15:44:00 +0100

    Add Terraform provisioning

---
+++ terraform/main.tf
@@ -0,0 +1,77 @@
+provider "google" {
+  version = "~> 3.6"
+}
+
+resource "google_cloud_run_service" "comingsoon" {
+  project  = google_project.root.project_id
+  location = "europe-west1"
+  name     = "comingsoon-animeshon-com"
+
+  template {
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "5"
+        "run.googleapis.com/client-name"   = "cloud-console"
+      }
+    }
+
+    spec {
+      container_concurrency = 80
+      service_account_name  = data.google_compute_default_service_account.root.email
+
+      containers {
+        image = "gcr.io/gcp-animeshon/comingsoon@sha256:118ef31eae6d5a2f4256f798d18b15392e54934ce3a534429a191ff3c6ef9192"
+
+        env {
+          name  = "HOST"
+          value = "comingsoon.animeshon.com"
+        }
+
+        resources {
+          limits = {
+            cpu    = "1000m"
+            memory = "256Mi"
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Configure the domain name mapping for the instance to comingsoon.animeshon.com.
+resource "google_cloud_run_domain_mapping" "comingsoon" {
+  project  = google_cloud_run_service.comingsoon.project
+  location = google_cloud_run_service.comingsoon.location
+  name     = "comingsoon.animeshon.com"
+
+  metadata {
+    namespace = google_project.root.project_id
+  }
+
+  spec {
+    route_name = google_cloud_run_service.comingsoon.name
+  }
+}
+
+# Allow everyone to access this instance from comingsoon.animeshon.com.
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "comingsoon" {
+  project  = google_cloud_run_service.comingsoon.project
+  location = google_cloud_run_service.comingsoon.location
+  service  = google_cloud_run_service.comingsoon.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
---
+++ terraform/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cat > $TF_CLI_CONFIG_FILE <<EOF
+credentials "app.terraform.io" {
+  token = "${TERRAFORM_CLI_TOKEN}"
+}
+EOF
+
+exit 0
