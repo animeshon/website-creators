440ca7c8578ddd7cce54f66736e44ec64f2ddbf6
Christian Roggia <christian.roggia@gmail.com>
2020-05-12 23:30:16 +0200

    Add DNS records

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,6 +9,19 @@ terraform {
   }
 }
 
+data "terraform_remote_state" "root" {
+  backend = "remote"
+
+  config = {
+    hostname = "app.terraform.io"
+    organization = "animeshon"
+
+    workspaces = {
+      name = "master"
+    }
+  }
+}
+
 data "terraform_remote_state" "general" {
   backend = "remote"
 
---
+++ terraform/dns.tf
@@ -0,0 +1,19 @@
+locals {
+  managed_zone_project_id = data.terraform_remote_state.root.outputs.project_id
+  managed_zone_dns_name   = data.terraform_remote_state.root.outputs.managed_zone_animeshon_com_dns_name
+  managed_zone_name       = data.terraform_remote_state.root.outputs.managed_zone_animeshon_com_name
+}
+
+# Setup comingsoon subdomain managed by Google Cloud Run.
+resource "google_dns_record_set" "comingsoon" {
+  project = local.managed_zone_project_id
+
+  name         = "comingsoon.${local.managed_zone_dns_name}"
+  managed_zone = local.managed_zone_name
+  type         = "CNAME"
+  ttl          = 300
+
+  rrdatas = [
+    "ghs.googlehosted.com.",
+  ]
+}
